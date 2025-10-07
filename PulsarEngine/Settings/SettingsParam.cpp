5d319f3e22507d8fafab371702e99a02679dfddf
Watcher <tobyh808@gmail.com>
2025-10-07 20:59:06 +0100

    Update SettingsParam.cpp

--- PulsarEngine/Settings/SettingsParam.cpp
+++ PulsarEngine/Settings/SettingsParam.cpp
@@ -8,11 +8,11 @@ namespace Pulsar {
 namespace Settings {
 
 u8 Params::radioCount[Params::pageCount] ={
-    3, 5, 3, 5, 2 //menu, race, host, OTT, KO
+    3, 5, 3, 5, 2, 1, 1 //menu, race, host, OTT, KO, WDDTC, WDDLAP
     //Add user radio count here
 
 };
-u8 Params::scrollerCount[Params::pageCount] ={ 1, 1, 1, 0, 2 }; //menu, race, host, OTT, KO
+u8 Params::scrollerCount[Params::pageCount] ={ 1, 1, 1, 0, 2, 2, 1 }; //menu, race, host, OTT, KO, WDDTC, WDDLAP
 
 u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] = //first row is PulsarSettingsType, 2nd is rowIdx of radio
 {
@@ -21,6 +21,8 @@ u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] = //fi
     { 2, 4, 2, 0, 0, 0 }, //Host
     { 3, 3, 2, 2, 2, 0 }, //OTT
     { 2, 2, 0, 0, 0, 0 }, //KO
+    { 2, 0, 0, 0, 0, 0 }, //WDDTC
+    { 2, 0, 0, 0, 0, 0 }, //WDDLAP
     //{}, //User
 };
 
@@ -31,6 +33,8 @@ u8 Params::optionsPerPagePerScroller[Params::pageCount][Params::maxScrollerCount
     { 7, 0, 0, 0, 0}, //Host
     { 0, 0, 0, 0, 0}, //OTT
     { 4, 4, 0, 0, 0}, //KO
+    { 4, 4, 0, 0, 0}, //WDDTC
+    { 9, 0, 0, 0, 0}, //WDDLAP
     //{}, //User
 };
 
