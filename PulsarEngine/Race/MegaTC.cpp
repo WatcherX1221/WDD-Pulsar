ff563a27f2f9656c3ccf36dd6cd647c2b365813e
Watcher <tobyh808@gmail.com>
2025-10-07 20:55:49 +0100

    Update MegaTC.cpp
    Custom TCs

--- PulsarEngine/Race/MegaTC.cpp
+++ PulsarEngine/Race/MegaTC.cpp
@@ -8,16 +8,70 @@ namespace Pulsar {
 namespace Race {
 //Mega TC
 void MegaTC(Kart::Movement& movement, int frames, int unk0, int unk1) {
-    if(System::sInstance->IsContext(PULSAR_MEGATC)) movement.ActivateMega();
-    else movement.ApplyLightningEffect(frames, unk0, unk1);
+    switch (System::sInstance->IsContext(WDD_TC_EFFECT)) {
+        case(0x1): //Double Effect
+            switch (System::sInstance->IsContext(WDD_TC_BIN1)+System::sInstance->IsContext(WDD_TC_BIN2)*2) {
+                case(0x1): //Mega
+                    movement.ActivateMega();
+                    break;
+                case(0x2): //Star
+                    movement.ActivateStar();
+                    break;
+                case(0x3): //Mushroom
+                    movement.ApplyInk(0);
+                    break;
+                default: //Default
+                    movement.ApplyLightningEffect(frames, unk0, unk1);
+            }
+            switch (System::sInstance->IsContext(WDD_EXTRATC_BIN1)+System::sInstance->IsContext(WDD_EXTRATC_BIN2)*2) {
+                case(0x1): //Mega
+                    movement.ActivateMega();
+                    break;
+                case(0x2): //Star
+                    movement.ActivateStar();
+                    break;
+                case(0x3): //Mushroom
+                    movement.ApplyInk(0);
+                    break;
+                default: //Default
+                    movement.ApplyLightningEffect(frames, unk0, unk1);
+            }
+        default: //Single Effect
+            switch (System::sInstance->IsContext(WDD_TC_BIN1)+System::sInstance->IsContext(WDD_TC_BIN2)*2) {
+                case(0x1): //Mega
+                    movement.ActivateMega();
+                    break;
+                case(0x2): //Star
+                    movement.ActivateStar();
+                    break;
+                case(0x3): //Mushroom
+                    movement.ApplyInk(0);
+                    break;
+                default: //Default
+                    movement.ApplyLightningEffect(frames, unk0, unk1);
+            }
+    }
 }
 kmCall(0x80580630, MegaTC);
 
 void LoadCorrectTCBRRES(Item::ObjKumo& objKumo, const char* mdlName, const char* shadowSrc, u8 whichShadowListToUse,
     Item::Obj::AnmParam* anmParam) {
-    if(System::sInstance->IsContext(PULSAR_MEGATC)) objKumo.LoadGraphics("megaTC.brres", mdlName, shadowSrc, 1, anmParam,
-        static_cast<nw4r::g3d::ScnMdl::BufferOption>(0), nullptr, 0);
-    else objKumo.LoadGraphicsImplicitBRRES(mdlName, shadowSrc, 1, anmParam, static_cast<nw4r::g3d::ScnMdl::BufferOption>(0), nullptr);
+    switch (System::sInstance->IsContext(WDD_TC_BIN1)+System::sInstance->IsContext(WDD_TC_BIN2)*2) {
+        case(0x1): //Mega
+            objKumo.LoadGraphics("megaTC.brres", mdlName, shadowSrc, 1, anmParam,
+            static_cast<nw4r::g3d::ScnMdl::BufferOption>(0), nullptr, 0);
+            break;
+        case(0x2): //Star
+            objKumo.LoadGraphics("starTC.brres", mdlName, shadowSrc, 1, anmParam,
+            static_cast<nw4r::g3d::ScnMdl::BufferOption>(0), nullptr, 0);
+            break;
+        case(0x3): //Blooper
+            objKumo.LoadGraphics("blooperTC.brres", mdlName, shadowSrc, 1, anmParam,
+            static_cast<nw4r::g3d::ScnMdl::BufferOption>(0), nullptr, 0);
+            break;
+        default: //Shock
+            objKumo.LoadGraphicsImplicitBRRES(mdlName, shadowSrc, 1, anmParam, static_cast<nw4r::g3d::ScnMdl::BufferOption>(0), nullptr);
+    }
 }
 kmCall(0x807af568, LoadCorrectTCBRRES);
 
